4e2fce297c01f04e83f448468b228307016ece7b
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-09-14 20:45:24 +0530

    Update streamlit_app.py
    request with json

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
 
