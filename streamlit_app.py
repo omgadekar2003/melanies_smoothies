2ba02cfb72eca01dd5a8a6de52442dda4afd30d4
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-09-15 09:50:32 +0530

    Update streamlit_app.py
    🥋 Make a Version of my_dataframe, but call it pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session  # Import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -51,9 +52,12 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:',my_dataframe, max_selections =5)
